6cccad879c5410386c4cf7c79dfeb5a8046cca2a
amandeep10 <amanjaura10@gmail.com>
2015-08-27 18:53:49 +0530

    first commit

---
+++ test.php
@@ -0,0 +1,15 @@
+<?php
+namespace lib;
+class autoload {
+	public function __construct(){
+		 spl_autoload_register(array($this, 'loader'));
+	}
+	private function loader($className) {
+		echo 'Trying to load ', $className, ' via ', __METHOD__, "()\n";
+		include $className . '.php';
+	}
+} 
+
+$autoloader = new ClassAutoloader();
+$obj = new Class1();
+$obj = new Class2();
